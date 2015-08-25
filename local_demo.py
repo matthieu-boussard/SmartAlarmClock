78eaeb3ae6cb73cef51c58f38f9df1c962803b28
Matthieu Boussard <matthieu.boussard@masagroup.net>
2015-08-25 16:25:30 +0200

    use https for accessing runtime

--- local_demo.py
+++ local_demo.py
@@ -64,7 +64,7 @@ os.environ["CRAFT_DEMO_SAC_APP_SECRET"] = config['sac_app_secret']
 os.environ["CRAFT_DEMO_SAC_PORT"] = '8080'
 os.environ["CRAFT_DEMO_SAC_URL"] = 'http://localhost:8080'
 os.environ["CRAFT_DEMO_SAC_WS_URL"] = 'ws://localhost:8080'
-os.environ["CRAFT_RUNTIME_SERVER_URL"] = 'http://runtime.craft.ai'
+os.environ["CRAFT_RUNTIME_SERVER_URL"] = 'https://runtime.craft.ai'
 os.environ["CRAFT_DEMO_SAC_ACTIONS_URL"] = public_url
 
 subprocess.call(["python", "-u", "src/server/main.py"])

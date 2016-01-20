1c5290af86a70e841d390b5af461c10d46d43ba6
Matthieu Boussard <matthieu.boussard@craft.ai>
2016-01-20 14:42:12 +0100

    use appID appSecret In success call

--- src/actions/DebugAction.py
+++ src/actions/DebugAction.py
@@ -4,10 +4,16 @@ import runtime
 import actions
 import json
 import requests
+import os
 
 defaultInputParams = {'message': '', 'value': 0.}
 defaultOutputParams = {}
 sim_parameters = dict()
+
+SAC_APP_SECRET = os.getenv('CRAFT_DEMO_SAC_APP_SECRET', '')
+SAC_APP_ID     = os.getenv('CRAFT_DEMO_SAC_APP_ID', '')
+
+HEADER_WITH_SECRETS = {'X-Craft-Ai-App-Id': SAC_APP_ID, 'X-Craft-Ai-App-Secret': SAC_APP_SECRET, 'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 # Register actions
 def registerAction(user, project, version, sim_id):
@@ -23,12 +29,10 @@ def start():
     request_Id = request.json['requestId']
 
     inputParams = actions.applyDefaultValues(inputParams, defaultInputParams)
-
     print 'Debuging:',inputParams['message'], ':', inputParams['value']
     success_url = '{}/v1/{}/{}/{}/{}/actions/{}/success'.format(runtime.CRAFT_RUNTIME_SERVER_URL, sim_parameters['user'],sim_parameters['project'],sim_parameters['version'],sim_parameters['sim_id'], request_Id)
-    json_headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-
-    r = requests.post(success_url)
+  
+    r = requests.post(success_url, headers=HEADER_WITH_SECRETS)
     return 
 
 
@@ -36,5 +40,5 @@ def cancel():
     agentId = int(request.json['agentId'])
     request_Id = request.json['requestId']
     cancel_url = '{}/v1/{}/{}/{}/{}/actions/{}/cancelation'.format(runtime.CRAFT_RUNTIME_SERVER_URL, sim_parameters['user'],sim_parameters['project'],sim_parameters['version'],sim_parameters['sim_id'], request_Id)
-    r = requests.post(cancel_url)
+    r = requests.post(cancel_url, headers=HEADER_WITH_SECRETS)
     return 
